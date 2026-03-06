f20f5f60e3c2c6906f42967728e825145242474e
Prince Cyrus Andres M. Ang <princecyrusandres.ang@neu.edu.ph>
2026-03-06 12:07:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9406975188-1a63d",
+  "appId": "1:965492997886:web:f52e8a6dafe652bf2cefaf",
+  "apiKey": "AIzaSyBlEq8namaq1Dx4tU0wqJiQ0ktQ_H_5fFw",
+  "authDomain": "studio-9406975188-1a63d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "965492997886"
+};
